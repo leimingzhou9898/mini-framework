70989d9dcebbb9e6471e264713c2bf1bc73cb679
leimingzhou9898 <1015230302@qq.com>
2019-01-06 17:34:27 +0800

    Update DemoService.java

--- springmvc-framework/src/com/demo/service/impl/DemoService.java
+++ springmvc-framework/src/com/demo/service/impl/DemoService.java
@@ -2,7 +2,7 @@ package com.demo.service.impl;
 
 import com.mvcframework.annotation.DMService;
 
-@DMService//(value="")
+@DMService
 public class DemoService implements IDemoService {
 
 	public String get(String name) {
